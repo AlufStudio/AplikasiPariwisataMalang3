b4096fe1749976882db5e86a3a99a33115046c09
SMK Telkom SP Malang <daffaattariq19@gmail.com>
2016-11-20 22:42:42 +0700

    help

---
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl208182838/aplikasipariwisatamalang3/HelpActivity.java
@@ -0,0 +1,16 @@
+package id.sch.smktelkom_mlg.project.xiirpl208182838.aplikasipariwisatamalang3;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+/**
+ * Created by SMK Telkom SP Malang on 20/11/2016.
+ */
+public class HelpActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.about);
+    }
+}
