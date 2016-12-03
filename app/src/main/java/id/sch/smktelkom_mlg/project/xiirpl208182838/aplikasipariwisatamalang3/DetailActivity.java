61566e5540fcfc9837b91c25510f3573578f5210
SMK Telkom SP Malang <daffaattariq19@gmail.com>
2016-12-03 21:24:08 +0700

    penambahan kritik saran menuju email di detail activity dan pengahpusan toast

--- app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl208182838/aplikasipariwisatamalang3/DetailActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl208182838/aplikasipariwisatamalang3/DetailActivity.java
@@ -1,12 +1,17 @@
 package id.sch.smktelkom_mlg.project.xiirpl208182838.aplikasipariwisatamalang3;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import model.Hotel;
 
@@ -36,13 +41,42 @@ public class DetailActivity extends AppCompatActivity {
             }
         });
 
-//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 //                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
 //                        .setAction("Action", null).show();
-//            }
-//        });
+//                protected void sendEmail() {
+
+                    Log.i("Send email", "");
+                    String[] TO = {"daffaattariq19@gmail.com"};
+                    String[] CC = {""};
+
+
+                    Intent emailIntent = new Intent(Intent.ACTION_SEND);
+
+                    emailIntent.setData(Uri.parse("mailto:"));
+                    emailIntent.setType("text/plain");
+                    emailIntent.putExtra(Intent.EXTRA_EMAIL, TO);
+                    emailIntent.putExtra(Intent.EXTRA_CC, CC);
+                    emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Your subject");
+                    emailIntent.putExtra(Intent.EXTRA_TEXT, "Email message goes here");
+
+
+                    try {
+                        emailIntent.setType("message/rfc822");
+
+                        startActivity(Intent.createChooser(emailIntent, "Send mail..."));
+                        finish();
+
+                        Log.i("Finished send email", "");
+
+                    } catch (android.content.ActivityNotFoundException ex) {
+//                        Toast.makeText(DetailActivity.this, "There is no email client installed.", Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+        });
     }
 }
